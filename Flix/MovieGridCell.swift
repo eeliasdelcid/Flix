9e3bac44313d2cec2a9829a506505e59a6b28afc
Ervin Elias <ervinjr011@gmail.com>
2020-02-06 20:41:33 -0800

    yo

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Ervin Del Cid on 2/6/20.
+//  Copyright © 2020 Ervin Del Cid. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
